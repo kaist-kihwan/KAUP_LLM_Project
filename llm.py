4baaab0fabf620d97fb2f48c84e4dcd9f5044a06
kaist-kihwan <kimkihwan@kaist.ac.kr>
2024-07-06 01:03:24 +0900

    add simple chatting that can decide respond's content

--- llm.py
+++ llm.py
@@ -17,33 +17,42 @@ from langchain_core.output_parsers import StrOutputParser
 
 prompt_template = PromptTemplate.from_template(
 """
-Please provide most correct answer from the following context. 
-If the answer is not present in the context, please write "The information is not present in the context."
----
-Question: {Question}
+As a helpful assistant,
+please use your best judgment to determine proper code for human's repond based on the following context.
+Please answer simple as you can.
+If you need more information to determine, please write "I need more explanation. Can you explain it more specifically?"
 ---
 Context: {Context}
+---
+Respond: It seems like a cat.
+Output: Human's respond is cat. Cat is an animal. The proper coding for cat is 'A'. Therfore, the output is 'A'.
+
+Respond: {Respond}
+Output:
 """
 )
+
+context = """
+'H' means human. If human's respond is similar to human, then proper coding of content must be 'H'.
+'A' means animal. If human's respond is similar to animal, then proper coding of content must be 'A'.
+'Fd' means food. If human's respond is similar to food, then proper coding of content must be 'Fd'.
+"""
 
 
 class Robot:
     def __init__(self, key:str):
         llm = ChatUpstage(api_key=key)
-        embeddings_model = UpstageEmbeddings(api_key=key, model="solar-embedding-1-large")
+        #embeddings_model = UpstageEmbeddings(api_key=key, model="solar-embedding-1-large")
         self.chain = prompt_template | llm | StrOutputParser()
 
     def _req_more_info(self):
         pass
 
-    def start(card_num:int):
-        pass
+    def run(self, message:str):
 
-    def run(self, message:str) -> str:
+        answer = self.chain.invoke({"Respond":message, "Context":context})
+        return answer
 
-        context = ""
-
-        answer = self.chain.invoke({"Question":"", "Context":context})
 
     def quit(self):
         pass
--- main.py
+++ main.py
@@ -25,10 +25,14 @@ def send_user_message():
         _print_message("User", message)
         entry.delete(0, tk.END)
 
+        ans = robot.run(message) # wip
+        
+        _print_message("Robot", ans)
 
-def send_robot_message():
-    message = robot.run("") # wip
-    _print_message("Robot", message)
+
+def show_result():
+    chatbot_frame.pack_forget()
+    result_frame.pack(fill='both', expand=True)
 
 
 def on_quit():
@@ -37,32 +41,10 @@ def on_quit():
 
 
 if __name__ == "__main__":
+
     # Set up the main application window
     root = tk.Tk()
     root.title("NEED NAME")
-
-    image_path = "image/Card_1.png"  # Ensure this is a path to a GIF or PPM/PGM image
-    original_image = tk.PhotoImage(file=image_path)
-
-    # Create a label widget to display the image
-    scaled_image = original_image.subsample(3, 3)
-    image_label = tk.Label(root, image=scaled_image)
-    image_label.pack(padx=10, pady=10)
-
-    # Create a scrolled text widget for the chat window
-    chat_window = scrolledtext.ScrolledText(root, wrap=tk.WORD, state=tk.DISABLED)
-    chat_window.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
-
-    # Create an entry widget for typing messages
-    entry = tk.Entry(root, width=80)
-    entry.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.X, expand=True)
-
-    # Create a button for sending messages
-    send_button = tk.Button(root, text="Send", command=send_user_message)
-    send_button.pack(side=tk.RIGHT, padx=10, pady=10)
-
-    # Bind key press events to the on_key_press function
-    root.bind('<Return>', lambda ev: send_user_message())
 
     # Bind the window close event to the custom function
     root.protocol("WM_DELETE_WINDOW", on_quit)
@@ -71,6 +53,44 @@ if __name__ == "__main__":
     load_dotenv()
     API_KEY = os.getenv('SECRET_KEY')
     robot = Robot(API_KEY)
+
+    ################################################ Chatbot #####################################################
+
+    chatbot_frame = tk.Frame(root)
+    chatbot_frame.pack(fill='both', expand=True)
+
+    image_path = "image/Card_1.png"  # Ensure this is a path to a GIF or PPM/PGM image
+    original_image = tk.PhotoImage(file=image_path)
+
+    # Create a label widget to display the image
+    scaled_image = original_image.subsample(3, 3)
+    image_label = tk.Label(chatbot_frame, image=scaled_image)
+    image_label.pack(padx=10, pady=10)
+
+    # Create a scrolled text widget for the chat window
+    chat_window = scrolledtext.ScrolledText(chatbot_frame, wrap=tk.WORD, state=tk.DISABLED)
+    chat_window.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+    # Create an entry widget for typing messages
+    entry = tk.Entry(chatbot_frame, width=80)
+    entry.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.X, expand=True)
+
+    # Create a button for sending messages
+    send_button = tk.Button(chatbot_frame, text="Send", command=send_user_message)
+    send_button.pack(side=tk.RIGHT, padx=10, pady=10)
+
+    # Bind key press events to the on_key_press function
+    root.bind('<Return>', lambda ev: send_user_message())
+
+    ################################################ Result #####################################################
+
+    result_frame = tk.Frame(root)
+
+
+
+    ################################################ Main Loop ##################################################
+
+    
 
     # Start the main event loop
     root.mainloop()
